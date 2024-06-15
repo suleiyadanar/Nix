e143602182dada622775cf8c3a31a0a60292d553
Grace <graceyang@brandeis.edu>
2024-06-15 15:48:33 -0400

    added empty mainrulesview

---
+++ Nix/Views/MainRulesView.swift
@@ -0,0 +1,21 @@
+//
+//  MainRulesView.swift
+//  Nix
+//
+//  Created by Grace Yang on 6/15/24.
+//
+
+import SwiftUI
+
+struct MainRulesView: View {
+    
+    var userId : String
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainRulesView(userId: "Grace")
+}
