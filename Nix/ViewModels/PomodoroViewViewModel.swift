21fbdc8bc7432e8eb7a898b27b3da412f096070d
suleiyadanar <sulei@brandeis.edu>
2024-06-28 22:39:15 -0400

    pomodoro breaks

--- Nix/ViewModels/PomodoroViewViewModel.swift
+++ Nix/ViewModels/PomodoroViewViewModel.swift
@@ -12,21 +12,42 @@ class PomodoroViewViewModel : NSObject,ObservableObject, UNUserNotificationCente
     @Published var progress: CGFloat = 1
     @Published var timerStringValue: String = "00:00"
     @Published var isStarted: Bool = false
+    @Published var isPaused: Bool = false
+
     @Published var addNewTimer: Bool = false
+
+    @Published var isBreak : Bool = false
+    @Published var pauseBreak: Bool = false
     
-    
-    
-    @Published var minutes: Int = 0
+    @Published var staticMinutes: Int = 25
+    @Published var staticSeconds: Int = 0
+    @Published var minutes: Int = 25
     @Published var seconds: Int = 0
     
     @Published var totalSeconds: Int = 0
     @Published var staticTotalSeconds: Int = 0
     
+    @Published var staticBreakMinutes: Int = 5
+    @Published var staticBreakSeconds: Int = 0
+    @Published var breakMinutes: Int = 5
+    @Published var breakSeconds: Int = 0
+    
+//    @Published var totalBreakSeconds: Int = 0
+//    @Published var staticTotalBreakSeconds: Int = 0
+//    
     @Published var isFinished: Bool = false
     
     override init() {
         super.init()
         self.authorizeNotification()
+        timerStringValue = "\(minutes >= 10 ? "\(minutes)":"0\(minutes)"):\(seconds >= 10 ? "\(seconds)":"0\(seconds)")"
+                
+        totalSeconds = (minutes * 60) + seconds
+        staticTotalSeconds = totalSeconds
+        
+//        totalBreakSeconds = (breakMinutes * 60) + seconds
+//        staticTotalBreakSeconds = totalBreakSeconds
+
     }
     func authorizeNotification() {
         UNUserNotificationCenter.current().requestAuthorization(options: [.sound, .alert, .badge]) { _, _ in }
@@ -41,6 +62,18 @@ class PomodoroViewViewModel : NSObject,ObservableObject, UNUserNotificationCente
             NotificationCenter.default.post(name: NSNotification.Name("NotificationTapped"), object: nil)
             completionHandler()
         }
+    func startBreak () {
+        withAnimation(.easeInOut(duration: 0.25)){
+            isBreak = true
+            isStarted = true
+        }
+        timerStringValue = "\(breakMinutes >= 10 ? "\(breakMinutes)":"0\(breakMinutes)"):\(breakSeconds >= 10 ? "\(breakSeconds)":"0\(breakSeconds)")"
+        
+        totalSeconds = (breakMinutes * 60) + breakSeconds
+        staticTotalSeconds = totalSeconds
+        addNotification()
+
+    }
     func startTimer() {
         withAnimation(.easeInOut(duration: 0.25)){
             isStarted = true
@@ -52,18 +85,6 @@ class PomodoroViewViewModel : NSObject,ObservableObject, UNUserNotificationCente
         staticTotalSeconds = totalSeconds
         addNewTimer = false
         addNotification()
-    }
-    
-    func stopTimer() {
-        withAnimation {
-            isStarted = false
-            minutes = 0
-            seconds = 0
-            progress = 1
-        }
-        totalSeconds = 0
-        staticTotalSeconds = 0
-        timerStringValue = "00:00"
     }
     
     func updateTimer() {
@@ -82,10 +103,74 @@ class PomodoroViewViewModel : NSObject,ObservableObject, UNUserNotificationCente
         }
     }
     
+    func changeState() {
+        if isBreak {
+            withAnimation  {
+                isBreak = false
+                minutes = staticMinutes
+                seconds = staticSeconds
+                totalSeconds = (staticMinutes * 60) + staticSeconds
+                timerStringValue = "\(staticMinutes >= 10 ? "\(staticMinutes)":"0\(staticMinutes)"):\(staticSeconds >= 10 ? "\(staticSeconds)":"0\(staticSeconds)")"
+//                staticTotalSeconds = totalSeconds
+            }
+         
+        } else {
+            withAnimation {
+                isBreak = true
+                breakMinutes = staticBreakMinutes
+                breakSeconds = staticBreakSeconds
+                totalSeconds = (staticBreakMinutes * 60) + staticBreakSeconds
+
+                timerStringValue = "\(staticBreakMinutes >= 10 ? "\(staticBreakMinutes)":"0\(staticBreakMinutes)"):\(staticBreakSeconds >= 10 ? "\(staticBreakSeconds)":"0\(staticBreakSeconds)")"
+            }
+         
+        }
+    }
+    
+    func pauseTimer() {
+        isStarted = false
+    }
+    func stopTimer() {
+        withAnimation {
+            isStarted = false
+            minutes = 0
+            seconds = 0
+            progress = 1
+        }
+        totalSeconds = 0
+        staticTotalSeconds = 0
+        timerStringValue = "00:00"
+    }
+    
+    func saveSettings() {
+          
+          minutes = staticMinutes
+          seconds = staticSeconds
+          
+          breakMinutes = staticBreakMinutes
+          breakSeconds = staticBreakSeconds
+          
+          timerStringValue = "\(minutes >= 10 ? "\(minutes)":"0\(minutes)"):\(seconds >= 10 ? "\(seconds)":"0\(seconds)")"
+          
+          totalSeconds = (minutes * 60) + seconds
+          
+//          totalBreakSeconds = (breakMinutes * 60) + seconds
+          
+          staticTotalSeconds = totalSeconds
+//          staticTotalBreakSeconds = totalBreakSeconds
+          addNewTimer = false
+      }
+    
+   
+    
     func addNotification() {
         let content = UNMutableNotificationContent()
         content.title = "Nix"
-        content.subtitle = "Pomodoro Complete"
+        if (isBreak) {
+            content.subtitle = "Break End"
+        }else{
+            content.subtitle = "Pomodoro Complete"
+        }
         content.sound = UNNotificationSound.default
         
         let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: UNTimeIntervalNotificationTrigger(timeInterval: TimeInterval(staticTotalSeconds), repeats: false))
